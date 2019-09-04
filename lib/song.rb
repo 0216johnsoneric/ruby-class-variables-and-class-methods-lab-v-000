2cbc1be6a9837a509d92e1c42dbf011b7ead2988
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-04 17:18:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,63 @@
-class Song
-  attr_accessor :name, :artists, :genre
-  attr_reader :artist
+# class Song
+#   attr_accessor :name, :artists, :genre
+#   attr_reader :artist
+#
+#   @@count = 0
+#   @@genres = []
+#   @@artists = []
+#   @@genre_count = {}
+#   @@artist_count = {}
+#
+#   def initialize(name, artist, genre)
+#     @@count += 1
+#     @@genres << genre
+#     @artist = artist
+#     @@artists << artist
+#     @name = name
+#     @genre = genre
+#   end
+#
+#   def self.count
+#     @@count
+#   end
+#
+#   def self.artists
+#     @@artists.uniq
+#   end
+#
+#   def self.genres
+#     @@genres.uniq
+#   end
+#
+#   def self.genre_count
+#     @@genres.each do |genre|
+#       @@genre_count[genre] = @@genres.count(genre)
+#       end
+#     @@genre_count
+#   end
+#
+#   def self.artist_count
+#     @@artists.each do |artist|
+#       @@artist_count[artist] = @@artists.count(artist)
+#     end
+#      @@artist_count
+#   end
+# end
 
+class Song
+  attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
   @@genre_count = {}
-  @@artist_count = {}
 
   def initialize(name, artist, genre)
     @@count += 1
-    @@genres << genre
-    @artist = artist
-    @@artists << artist
     @name = name
+    @artist = artist
     @genre = genre
+    @@artists << artist
+    @@genres << genre
   end
 
   def self.count
@@ -30,16 +73,5 @@ class Song
   end
 
   def self.genre_count
-    @@genres.each do |genre|
-      @@genre_count[genre] = @@genres.count(genre)
-      end
-    @@genre_count
-  end
-
-  def self.artist_count
-    @@artists.each do |artist|
-      @@artist_count[artist] = @@artists.count(artist)
-    end
-     @@artist_count
   end
 end
