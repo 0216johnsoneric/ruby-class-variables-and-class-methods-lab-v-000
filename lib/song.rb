457971a1cee2cec58e4259c19ed9d3324f96dd10
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-04 18:22:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,57 +45,57 @@
 # end
 require 'pry'
 
-class Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artists = []
-  @@genres = []
-  @@genre_count = {}
-  @@artist_count = {}
-
-  def initialize(name, artist, genre)
-    @@count += 1
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@artists << artist
-    @@genres << genre
-
-  end
-
-  def self.count
-    @@count
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.genre_count
-
-    @@genres.uniq.each do |genre_name|
-      @@genre_count[genre_name] = 0
-    end
-    @@genres.each do |genre_name|
-      @@genre_count[genre_name] += 1
-    end
-    @@genre_count
-  end
-
-  def self.artist_count
-
-    @@artists.uniq.each do |artist_name|
-      @@artist_count[artist_name] = 0
-    end
-    @@artists.each do |artist_name|
-      @@artist_count[artist_name] += 1
-    end
-    @@artist_count
-  end
+# class Song
+#   attr_accessor :name, :artist, :genre
+#   @@count = 0
+#   @@artists = []
+#   @@genres = []
+#   @@genre_count = {}
+#   @@artist_count = {}
+#
+#   def initialize(name, artist, genre)
+#     @@count += 1
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@artists << artist
+#     @@genres << genre
+#
+#   end
+#
+#   def self.count
+#     @@count
+#   end
+#
+#   def self.artists
+#     @@artists.uniq
+#   end
+#
+#   def self.genres
+#     @@genres.uniq
+#   end
+#
+#   def self.genre_count
+#
+#     @@genres.uniq.each do |genre_name|
+#       @@genre_count[genre_name] = 0
+#     end
+#     @@genres.each do |genre_name|
+#       @@genre_count[genre_name] += 1
+#     end
+#     @@genre_count
+#   end
+#
+#   def self.artist_count
+#
+#     @@artists.uniq.each do |artist_name|
+#       @@artist_count[artist_name] = 0
+#     end
+#     @@artists.each do |artist_name|
+#       @@artist_count[artist_name] += 1
+#     end
+#     @@artist_count
+#   end
 
 
 end
